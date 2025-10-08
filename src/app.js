cf77746561a31b4323f7412853f624b742b2d20e
Estimatus <122575833+Estimatus@users.noreply.github.com>
2025-10-08 13:16:47 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,28 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  const newExcuse = document.querySelector('#the-excuse').innerHTML = generateNewExcuse();
   console.log("Hello Rigo from the console!");
+  
 };
+
+const getRandomItemFromArray = (target) => {
+	const randomItem = Math.floor(Math.random() * target.length)
+	return target[randomItem]
+}
+
+const excuses = {
+  who: ['The dog', 'My grandma', 'The mailman', 'My bird'],
+  action: ['ate', 'peed', 'crushed', 'broke'],
+  what: ['my homework', 'my phone', 'the car'],
+  when: ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying']
+};
+
+  const generateNewExcuse = () => {
+	const who = getRandomItemFromArray(excuses?.who)
+	const action = getRandomItemFromArray(excuses?.action)
+	const what = getRandomItemFromArray(excuses?.what)
+  const when = getRandomItemFromArray(excuses?.when)
+	
+	return `${who} ${action} ${what} ${when}`
+}
